cd773f69417480290763f3abad84c1dd3abab3b9
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-06-12 13:10:45 -0400

    Modelos creados..

---
+++ RegistroMoraBlazor/Models/Mora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroMoraBlazor.Models
+{
+    public class Mora
+    {
+        [Key]
+        [Required(ErrorMessage = "El campo Id no puede estar vacío.")]
+        [Range(0, 1000000, ErrorMessage = "El campo Id no puede ser menor que cero o mayor a 1000000.")]
+        public int moraId { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Required(ErrorMessage = "El campo fecha no puede estar vacío.")]
+        [DisplayFormat(DataFormatString = "{0:dd,mm, yyyy}")]
+        public DateTime fecha { get; set; }
+
+        [Required(ErrorMessage = "El campo total no puede estar vacio.")]
+        [Range(1, 100000000, ErrorMessage = "El rango es de 1 a 1000000.")]
+        public decimal total { get; set; }
+       
+        [ForeignKey("moraId")]
+        public virtual List<MoraDetalle> MoraDetalles { get; set; }
+
+
+        public Mora()
+        {
+            moraId = 0;
+            fecha = DateTime.Now;
+            total = 0;
+        }
+    }
+}
---
+++ RegistroMoraBlazor/Models/MoraDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroMoraBlazor.Models
+{
+    public class MoraDetalle
+    {
+        [Key]
+        public int moraDetalleId { get; set; }
+        public int moraId { get; set; }
+        public int prestamoId { get; set; }
+        public decimal valor { get; set; }
+
+        public MoraDetalle()
+        {
+            moraDetalleId = 0;
+            moraId = 0;
+            prestamoId = 0;
+            valor = 0;
+        }
+    }
+}
