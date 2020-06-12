de56b077ffd2ec827d3f4f84aa3933e277a3fd5f
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-06-12 13:15:22 -0400

    Contexto creado.

---
+++ RegistroMoraBlazor/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroMoraBlazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroMoraBlazor.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=DATA\Mora.db");
+        }
+
+        public DbSet<Prestamo> Prestamos { get; set;}
+        public DbSet<Mora> Moras { get; set; }
+    }
+}
