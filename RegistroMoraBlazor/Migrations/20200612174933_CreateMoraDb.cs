1c8d95f34655dac8059df5b65b372a258bc45a0c
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-06-12 13:54:01 -0400

    Registro de prestamo creado..

---
+++ RegistroMoraBlazor/Migrations/20200612174933_CreateMoraDb.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistroMoraBlazor.Migrations
+{
+    public partial class CreateMoraDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Moras",
+                columns: table => new
+                {
+                    moraId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    fecha = table.Column<DateTime>(nullable: false),
+                    total = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moras", x => x.moraId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prestamos",
+                columns: table => new
+                {
+                    prestamoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    fecha = table.Column<DateTime>(nullable: false),
+                    concepto = table.Column<string>(maxLength: 40, nullable: false),
+                    monto = table.Column<decimal>(nullable: false),
+                    balance = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestamos", x => x.prestamoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MoraDetalle",
+                columns: table => new
+                {
+                    moraDetalleId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    moraId = table.Column<int>(nullable: false),
+                    prestamoId = table.Column<int>(nullable: false),
+                    valor = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoraDetalle", x => x.moraDetalleId);
+                    table.ForeignKey(
+                        name: "FK_MoraDetalle_Moras_moraId",
+                        column: x => x.moraId,
+                        principalTable: "Moras",
+                        principalColumn: "moraId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MoraDetalle_moraId",
+                table: "MoraDetalle",
+                column: "moraId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MoraDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Prestamos");
+
+            migrationBuilder.DropTable(
+                name: "Moras");
+        }
+    }
+}
